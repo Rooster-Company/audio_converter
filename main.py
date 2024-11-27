0c454459e998237444e8f28cb4bfcbe0bcb679c6
Guilherme <guilherme.santos.verissimo@gmail.com>
2024-11-27 15:13:02 -0300

    main

--- main.py
+++ main.py
@@ -32,5 +32,5 @@ async def convert(item: UrlAudio):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=os.getenv("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("PORT", 8000)))
     
