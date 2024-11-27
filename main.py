7bc5569f8a5a272944aa3a7309381b3579c259dc
Guilherme <guilherme.santos.verissimo@gmail.com>
2024-11-27 12:15:14 -0300

    convert

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
+from urllib.request import urlopen
+from pydub import AudioSegment
+from io import BytesIO
+from pydantic import BaseModel
+
+
+def convert_to_mp3(file_url):
+    file = BytesIO(urlopen(file_url).read())
+    converted = BytesIO()
+    AudioSegment.from_file(file).export(converted, format='mp3')
+    return converted
+
+
+class UrlAudio(BaseModel):
+    url: str
 
 app = FastAPI()
 
+
 @app.get("/")
 async def root():
-    return {"greeting": "Hello, World!", "message": "Welcome to FastAPI!"}
+    return {"greeting": "Hello, World!", "message": "Welcome to FastAPI audio converter!"}
+
+
+@app.post("/convert")
+async def convert(item: UrlAudio):
+    converted = convert_to_mp3(item.url)
+    return Response(content=converted, media_type="audio/mp3")
